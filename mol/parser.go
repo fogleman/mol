ed6b7e47c8088956a05b22ca5b6b57a432eef91d
Michael Fogleman <fogleman@gmail.com>
2016-02-16 14:24:53 -0500

    parser bug fix

--- mol/parser.go
+++ mol/parser.go
@@ -16,9 +16,8 @@ func ParseFile(path string) (Molecule, error) {
 
 func ParseString(data string) Molecule {
 	lines := strings.Split(data, "\n")
-	fields := strings.Fields(lines[3])
-	natoms := parseInt(fields[0])
-	nbonds := parseInt(fields[1])
+	natoms := parseInt(lines[3][0:3])
+	nbonds := parseInt(lines[3][3:6])
 	atoms := make([]Atom, natoms)
 	for i := 0; i < natoms; i++ {
 		fields := strings.Fields(lines[i+4])
@@ -30,21 +29,23 @@ func ParseString(data string) Molecule {
 	}
 	bonds := make([]Bond, nbonds)
 	for i := 0; i < nbonds; i++ {
-		fields := strings.Fields(lines[i+4+natoms])
-		a := parseInt(fields[0]) - 1
-		b := parseInt(fields[1]) - 1
-		t := parseInt(fields[2])
+		line := lines[i+4+natoms]
+		a := parseInt(line[0:3]) - 1
+		b := parseInt(line[3:6]) - 1
+		t := parseInt(line[6:9])
 		bonds[i] = Bond{a, b, t}
 	}
 	return Molecule{atoms, bonds}
 }
 
 func parseInt(x string) int {
+	x = strings.TrimSpace(x)
 	value, _ := strconv.ParseInt(x, 0, 0)
 	return int(value)
 }
 
 func parseFloat(x string) float64 {
+	x = strings.TrimSpace(x)
 	value, _ := strconv.ParseFloat(x, 64)
 	return value
 }
